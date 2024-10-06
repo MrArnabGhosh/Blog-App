544b73a7297f2e205ecff87bc0021e25c062c695
MrArnabGhosh <ghosharnab460@gmail.com>
2024-10-06 17:26:19 +0530

    ...

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from './pages/Home'
-import Signin from './pages/signin'
-import Signup from './pages/signup'
-import Dashboard from './pages/dashboard'
-import Projects from './pages/Projects'
-import About from './pages/about'
-import Header from './components/Header'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Signin from './pages/signin';
+import Dashboard from './pages/dashboard';
+import Projects from './pages/Projects';
+import About from './pages/about';
+import Header from './components/Header';
+import Signup from './pages/signup';
 
 export default function App() {
   return (
     <BrowserRouter>
-    <Header />
-      <Routes> {/* Corrected from Routs to Routes */}
-        <Route path="/ " element={<Home />} />
-        <Route path="/singin" element={<Signin />} /> 
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} /> {/* Removed extra space */}
+        <Route path="/signin" element={<Signin />} /> {/* Corrected "singin" to "signin" */}
         <Route path="/about" element={<About />} />
-        <Route path="/signup" element={<Signup />} /> 
-        <Route path="/dashboard" element={<Dashboard />} /> 
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
