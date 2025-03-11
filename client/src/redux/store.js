aa156ad5aa7daf69b03c024f8cd89f71e75790a7
MrArnabGhosh <ghosharnab460@gmail.com>
2025-03-12 00:58:51 +0530

    redux persist local storage

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import  userReducer  from './user/UserSlice'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+import { version } from 'mongoose'
+import persistStore from 'redux-persist/es/persistStore'
 
-export const store = configureStore({
-  reducer: {
-    user:userReducer,
-  },
+
+
+
+const rootReducer = combineReducers({
+  user:userReducer,
 })
+
+const persistConfig={
+    key:'root',
+    storage,
+    version:1,
+}
+const persistedReducer=persistReducer(persistConfig)
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware:(getDefaultMiddleware)=>
+    getDefaultMiddleware({serializableCheck: false}),
+})
+
+export const persistor = persistStore(store)
