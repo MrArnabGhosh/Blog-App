bd29a3e4b66c2f8d172c0144726d6eb35c692a2b
MrArnabGhosh <ghosharnab460@gmail.com>
2025-08-23 20:28:33 +0530

    show comment of the post

--- client/src/components/CommentSection.jsx
+++ client/src/components/CommentSection.jsx
@@ -1,12 +1,16 @@
 import { Alert, Button, Textarea } from 'flowbite-react'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import {useSelector} from 'react-redux'
 import {Link} from 'react-router-dom'
+import Comment from './Comment'
+
 
 export default function CommentSection({postId}) {
     const {currentUser} = useSelector(state=>state.user)
     const [comment, setComment] = useState('')
     const [commentError, setCommentError] = useState(null)
+    const [showComment, setShowComment] = useState([])
+
     const handelSubmit = async(e)=>{
       e.preventDefault()
       if(comment.length>200){
@@ -24,14 +28,29 @@ export default function CommentSection({postId}) {
         if(res.ok){
           setComment('')
           setCommentError(null)
+          setShowComment([data ,...showComment])
         }
   
       } catch (error) {
         setCommentError(error.message)
       }
     }
+    console.log(showComment);
 
-
+    useEffect(()=>{
+      const getComments = async()=>{
+        try {
+          const res = await fetch(`/api/comment/getPostComments/${postId}`)
+          if(res.ok){
+            const data = await res.json({})
+            setShowComment(data)
+          }
+        } catch (error) {
+          console.log(error)
+        }
+      }
+      getComments()
+    },[postId])
   return (
     <div className='max-w-xl mx-auto w-full p-3'>
       {currentUser ?
@@ -71,6 +90,21 @@ export default function CommentSection({postId}) {
         )}
         </form>
       )}
+      {showComment.length ===0 ? (
+          <p className='text-sm my-5'>No comments Yet!</p>
+      ) :(
+        <>
+         <div className='text-sm my-5 flex items-center gap-1'>
+          <p>Comments</p>
+          <div className='border border-gray-400 py-1 px-2 rounded-sm'>
+            <p>{showComment.length}</p>
+          </div>
+         </div>
+         {showComment.map(comment=>(
+          <Comment key={comment._id} comment={comment}/>
+         ))}
+         </>
+      ) }
     </div>
   )
 }
